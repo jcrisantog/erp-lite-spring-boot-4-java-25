afaf1d2749b7114d262b939bbea9535fbac9bc8f
jcrisantog <jcrisantog@gmail.com>
2026-09-13 17:36:17 -0600

    feat: Base de datos

--- erp-api/src/main/java/com/debuggeandoideas/erp_lite/ErpLiteApplication.java
+++ erp-api/src/main/java/com/debuggeandoideas/erp_lite/ErpLiteApplication.java
@@ -4,7 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ErpLiteApplication {
+public class
+ErpLiteApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ErpLiteApplication.class, args);
